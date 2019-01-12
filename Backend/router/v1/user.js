7ead0aa8c02544672bfe4ac4ae747ee08ac676d9
heeji <heeji@MacBook-Pro-3.local>
2019-01-11 16:39:10 -0800

    merge conflict

--- Backend/router/v1/user.js
+++ Backend/router/v1/user.js
@@ -22,14 +22,13 @@ router.post('/signup', (req, res) => {
     where: {
       email: req.body.email
     }
-=======
+    
   let hashPassword = crypto.createHash("sha512").update(req.body.password).digest("hex");
 
   models.user.create({
       email : req.body.email,
       password : hashPassword,
 
->>>>>>> 39504b49b82705b19cea3df31ba11c4fbddf59e8
   }).then(result => {
     if (result == "") {
 
