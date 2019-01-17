68fee6826f9c2a8028a0649713b6ace58e9b15ea
heeji <heeji@MacBook-Pro-3.local>
2019-01-17 15:13:36 -0800

    signin-issue21-22

--- Backend/router/v1/user.js
+++ Backend/router/v1/user.js
@@ -1,27 +1,63 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../../models');
-const crypto = require('crypto')
+const crypto = require('crypto');
+const session = require('express-session');
 
-router.use((req, res, next) => {
+router.use(session({
+  key: 'sid',
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24000*60*60
+  }
+}));
+
+
+router.use((req,res,next) => {
   console.log("user router");
   next();
 });
 
-router.all('/', (req, res) => {
+router.all('/', (req,res) => {
   res.send("this is user root");
 });
 
-router.post('/signup', (req, res) => {
-  let hashPassword = crypto.createHash("sha512").update(req.body.password).digest("hex");
+router.get('/signin', (req, res) => {
 
-  models.user.create({
-    email: req.body.email,
-    password: hashPassword,
+  let session = req.session;
+
+  models.user.findAll({
+    where: {
+      email: req.query.email
+    }
   }).then(result => {
-    console.log("everything is good");
-    res.send(result);
+    if (result=="")
+    {
+      console.log("User does not exit");
+      res.json({msg: "User does not exit"});
+    } else {
+      let dbPassword = result[0].dataValues.password;
+      let hashPassword = crypto.createHash("sha512").update(req.query.password).digest("hex");
+
+      if(dbPassword == hashPassword) {
+        console.log("Password Match");
+
+        req.session.email = req.query.email;
+        res.json({msg: req.session.email});
+
+      } else {
+        console.log("Password not Match");
+        res.json({msg: "Password not Match"});
+      };
+    };
+
+  }).catch( err => {
+    console.log(err);
+    res.json({msg: "err"});
   });
+
 });
 
 module.exports = router;
