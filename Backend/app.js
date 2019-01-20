e31f57bd9a6df4256b0cc37761e9836ad7737a62
LChoiSH <csh42504@google.com>
2019-01-19 17:20:26 -0800

    #48 signon function change for session check

--- Backend/app.js
+++ Backend/app.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
 const models = require('./models');
-const router = express.Router();
-const rootRouter = require('./router/index');
 const bodyParser = require('body-parser');
+const router = express.Router();
+const session = require('express-session');
 
 models.sequelize.sync();
+
+app.use(session({
+  secret: 'asadlkj!a@#a!@a#dfgasdg',
+  cookie: {maxAge: 60000},
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
+const passport = require('./lib/passport')(app);
+const rootRouter = require('./router/index')(passport);
+
 app.use('/api', rootRouter);
 
-app.listen(3000, () => {
+app.listen(80, () => {
   console.log("Server Start");
 });
